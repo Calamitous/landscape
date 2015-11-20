7c22155277948791433e6858419ea1b130e94b01
Eric Budd <eric.b.budd@gmail.com>
2015-11-19 23:58:44 -0500

    Refactoring

--- lib/landscape.ex
+++ lib/landscape.ex
@@ -2,77 +2,70 @@ defmodule Landscape do
   @width 50
   @height 15
 
+  # Position
+  defmacro top(elem), do: quote do: @width > unquote(elem)
+  defmacro bot(elem), do: quote do: @height <= (div(unquote(elem), @width) + 1)
+  defmacro bol(elem), do: quote do: rem(unquote(elem), @width) == 0
+  defmacro eol(elem), do: quote do: rem(unquote(elem) + 1, @width) == 0
+
   def elem_to_xy(elem),   do: {div(elem, @width), rem(elem, @width)}
   def xy_to_elem({x, y}), do: (@width * y) + x
 
-  def neighbor(elem, :n)  when elem > @width,                                                    do: elem - @width
-  def neighbor(elem, :ne) when elem > @width and rem(elem + 1, @width) != 0,                     do: (elem - @width) + 1
-  def neighbor(elem, :e)  when rem(elem + 1, @width) != 0,                                       do: elem + 1
-  def neighbor(elem, :se) when (div(elem, @width) + 1) < @height and rem(elem + 1, @width) != 0, do: elem + @width + 1
-  def neighbor(elem, :s)  when (div(elem, @width) + 1) < @height,                                do: elem + @width
-  def neighbor(elem, :sw) when (div(elem, @width) + 1) < @height and rem(elem, @width) != 0,     do: (elem + @width) - 1
-  def neighbor(elem, :w)  when rem(elem, @width) != 0,                                           do: elem - 1
-  def neighbor(elem, :nw) when elem > @width and rem(elem, @width) != 0,                         do: elem - 1 - @width
+  def neighbor(elem, :n)  when not top(elem),                   do: elem - @width
+  def neighbor(elem, :e)  when not eol(elem),                   do: elem + 1
+  def neighbor(elem, :s)  when not bot(elem),                   do: elem + @width
+  def neighbor(elem, :w)  when not bol(elem),                   do: elem - 1
+  def neighbor(elem, :ne) when not top(elem) and not eol(elem), do: (elem - @width) + 1
+  def neighbor(elem, :se) when not bot(elem) and not eol(elem), do: elem + @width + 1
+  def neighbor(elem, :sw) when not bot(elem) and not bol(elem), do: (elem + @width) - 1
+  def neighbor(elem, :nw) when not top(elem) and not bol(elem), do: elem - 1 - @width
   def neighbor(elem, _), do: elem
 
   def neighbors(elem),                               do: neighbors(elem, [:n, :ne, :e, :se, :s, :sw, :w, :nw], [])
   def neighbors(elem, [direction | tail], elements), do: neighbors(elem, tail, [neighbor(elem, direction)| elements])
   def neighbors(elem, [], elements),                 do: Enum.uniq(elements) -- [elem]
 
-  def neighbors(ma, {x, y}, distance \\ 1) do
-    x = x + 1
-    y = y + 1
-    [x1, y1, x2, y2] = boundary(x - distance, y - distance, x + distance, y + distance)
-    m = Enum.chunk(ma, @width)
-    Enum.map(Enum.slice(m, x1..x2), fn(row) -> Enum.slice(row, y1..y2) end)
-  end
-
-  def print_map(m, w \\ @width) do
-    flat_map = Enum.with_index(List.flatten(m))
-    Enum.each(flat_map, fn({c, i}) -> print_cell({c, rem(i + 1, w)}); end)
+  # Display
+  def print_map(m) do
+    map_w_index = Enum.with_index(m)
+    Enum.each(map_w_index, fn({c, i}) -> print_cell(c, eol(i)); end)
     m
   end
 
-  def has_potential_water(m, elem) do
-    m
-    |> neighbors(elem_to_xy(elem))
-    |> List.flatten()
-    # |> Enum.filter(fn({lev, _}) -> lev >= get_elevation(m, elem) end)
+  defp print_cell({c, false}, true), do: IO.puts(c)
+  defp print_cell({c, false}, _),    do: IO.write(c)
+  defp print_cell({c, true},  true), do: IO.puts("#{IO.ANSI.blue}#{c}#{IO.ANSI.reset}")
+  defp print_cell({c, true},  _),    do: IO.write("#{IO.ANSI.blue}#{c}#{IO.ANSI.reset}")
+
+  # Progression
+  def should_have_water(m, elem) do
+    m = List.to_tuple(m)
+    neighbors(elem)
+    |> Enum.map(fn(e) -> elem(m, e) end)
+    |> Enum.filter(fn({lev, _}) -> lev >= get_elevation(m, elem) end)
     |> Enum.map(fn(c) -> elem(c, 1) end)
     |> Enum.reduce(false, fn(c, acc) -> acc || c end)
   end
 
   def flow_water(m) do
     Enum.map Enum.with_index(m), fn({{elev, has_water}, i}) ->
-      {elev, has_water || has_potential_water(m, i)}
+      {elev, has_water || should_have_water(m, i)}
     end
   end
 
   def update_map(m), do: m |> flow_water
-  defp get_elevation(m, elem), do: elem(elem(List.to_tuple(m), elem), 0)
-  defp print_cell({{c, false}, 0}), do: IO.puts(c)
-  defp print_cell({{c, false}, _}), do: IO.write(c)
-  defp print_cell({{c, true},  0}), do: IO.puts("#{IO.ANSI.blue}#{c}#{IO.ANSI.reset}")
-  defp print_cell({{c, true},  _}), do: IO.write("#{IO.ANSI.blue}#{c}#{IO.ANSI.reset}")
+  defp get_elevation(m, elem), do: elem(elem(m, elem), 0)
 
+  # Generation
+  def add_water(m), do: add_water(m, :random.uniform(@width * @height))
   def add_water(m, elem) do
     {elev, _} = hd(Enum.slice(m, elem..elem))
     Enum.slice(m, 0..elem-1) ++ [{elev, true}] ++ Enum.slice(m, elem+1..length(m))
   end
 
-  def add_random_water(m), do: add_water(m, :random.uniform(@width * @height))
-
-  def create_elevations([_ | rest]), do: elevation() ++ create_elevations(rest)
-  def create_elevations([]),         do: []
-
-  defp elevation, do: [{:random.uniform(5), false}]
-
   def build_map,                                  do: build_map([])
   defp build_map(m) when length(m) == @width * @height, do: m
-  defp build_map(m) when length(m) < @width * @height,  do: build_map(m ++ [nil])
+  defp build_map(m) when length(m) < @width * @height,  do: build_map(m ++ elevation())
 
-  defp boundary(x, max), do: Enum.min([Enum.max([x, 0]), max])
-  defp boundary(x1, y1, x2, y2) do
-    [boundary(x1, @width), boundary(y1, @height), boundary(x2, @width), boundary(y2, @height)]
-  end
+  defp elevation, do: [{:random.uniform(5), false}]
 end
--- lib/sim.ex
+++ lib/sim.ex
@@ -1,27 +1,17 @@
 defmodule Sim do
   def run do
     Landscape.build_map
-    |> Landscape.create_elevations
-    |> Landscape.add_random_water
+    |> Landscape.add_water
     |> run(false)
   end
 
   def run(map, false) do
-    # :timer.sleep(1000)
-
     map = map
     |> Landscape.print_map
     |> Landscape.update_map
     |> run(should_stop(IO.getn('--------- q to quit: ')))
-
-    map
   end
 
-  def run(map, true) do
-    IO.puts 'Bye!'
-  end
-
-  def should_stop(foo) do
-    foo == "q"
-  end
+  def run(map, true), do: IO.puts 'Bye!'
+  def should_stop(key), do: key == "q"
 end
