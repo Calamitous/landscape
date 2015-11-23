b2e0ec3ca55bc651cb2aac928660f8d7bc34a8ed
Eric Budd <eric.b.budd@gmail.com>
2015-11-23 00:00:09 -0500

    Added foliage and refactored display code

--- lib/landscape.ex
+++ lib/landscape.ex
@@ -2,6 +2,12 @@ defmodule Landscape do
   @width 50
   @height 15
 
+  @water  "#{IO.ANSI.blue_background}#{IO.ANSI.black}"
+  @dirt   "#{IO.ANSI.magenta_background}#{IO.ANSI.black}"
+  @grass  "#{IO.ANSI.green_background}#{IO.ANSI.black}"
+  @flower "#{IO.ANSI.green_background}#{IO.ANSI.yellow}"
+  @reset  IO.ANSI.reset
+
   # Position
   defmacro top(elem), do: quote do: @width > unquote(elem)
   defmacro bot(elem), do: quote do: @height <= (div(unquote(elem), @width) + 1)
@@ -28,28 +34,31 @@ defmodule Landscape do
   # Display
   def print_map(m) do
     map_w_index = Enum.with_index(m)
-    Enum.each(map_w_index, fn({c, i}) -> print_cell(c, eol(i)); end)
+    Enum.each(map_w_index, fn({c, i}) -> print_cell(c, eol(i)) end)
     m
   end
 
-  defp print_cell({c, false}, true), do: IO.puts(c)
-  defp print_cell({c, false}, _),    do: IO.write(c)
-  defp print_cell({c, true},  true), do: IO.puts("#{IO.ANSI.blue}#{c}#{IO.ANSI.reset}")
-  defp print_cell({c, true},  _),    do: IO.write("#{IO.ANSI.blue}#{c}#{IO.ANSI.reset}")
+  defp print_cell({c, false, 0}, is_eol), do: smart_put("#{@dirt}#{c}#{@reset}", is_eol)
+  defp print_cell({c, false, 1}, is_eol), do: smart_put("#{@grass}#{c}#{@reset}", is_eol)
+  defp print_cell({c, false, 2}, is_eol), do: smart_put("#{@flower}#{c}#{@reset}", is_eol)
+  defp print_cell({c, true, _},  is_eol), do: smart_put("#{@water}#{c}#{@reset}", is_eol)
+
+  defp smart_put(str, true), do: IO.puts(str)
+  defp smart_put(str, _),    do: IO.write(str)
 
   # Progression
   def should_have_water(m, elem) do
     m = List.to_tuple(m)
     neighbors(elem)
     |> Enum.map(fn(e) -> elem(m, e) end)
-    |> Enum.filter(fn({lev, _}) -> lev >= get_elevation(m, elem) end)
+    |> Enum.filter(fn({lev, _, _}) -> lev >= get_elevation(m, elem) end)
     |> Enum.map(fn(c) -> elem(c, 1) end)
     |> Enum.reduce(false, fn(c, acc) -> acc || c end)
   end
 
   def flow_water(m) do
-    Enum.map Enum.with_index(m), fn({{elev, has_water}, i}) ->
-      {elev, has_water || should_have_water(m, i)}
+    Enum.map Enum.with_index(m), fn({{elev, has_water, grass}, i}) ->
+      {elev, has_water || should_have_water(m, i), grass}
     end
   end
 
@@ -59,13 +68,13 @@ defmodule Landscape do
   # Generation
   def add_water(m), do: add_water(m, :random.uniform(@width * @height))
   def add_water(m, elem) do
-    {elev, _} = hd(Enum.slice(m, elem..elem))
-    Enum.slice(m, 0..elem-1) ++ [{elev, true}] ++ Enum.slice(m, elem+1..length(m))
+    {elev, _, grass} = hd(Enum.slice(m, elem..elem))
+    Enum.slice(m, 0..elem-1) ++ [{elev, true, grass}] ++ Enum.slice(m, elem+1..length(m))
   end
 
   def build_map,                                  do: build_map([])
   defp build_map(m) when length(m) == @width * @height, do: m
   defp build_map(m) when length(m) < @width * @height,  do: build_map(m ++ elevation())
 
-  defp elevation, do: [{:random.uniform(5), false}]
+  defp elevation, do: [{:random.uniform(3) - 1, false, :random.uniform(3) - 1}]
 end
