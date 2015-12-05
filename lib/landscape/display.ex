aab1f1a5991504ebd1e3ac4ff91353e0c56e6ccb
Eric Budd <eric.b.budd@gmail.com>
2015-12-05 02:19:53 -0500

    Making fuller use of ANSI directives

--- lib/landscape/display.ex
+++ lib/landscape/display.ex
@@ -1,7 +1,15 @@
 defmodule Landscape.Display do
   require Landscape.Position
 
+  defmacro valid_rgb216(r, g, b) do
+    quote do
+      0 <= unquote(r) and 0 <= unquote(g) and 0 <= unquote(b) and
+        unquote(r) <= 5 and unquote(g) <= 5 and unquote(b) <= 5
+    end
+  end
   @reset  IO.ANSI.reset
+
+  def rgb216(r, g, b) when valid_rgb216(r, g, b), do: 16 + (36 * r) + (6 * g) + b
 
   def lava(_),         do: "#{IO.ANSI.red_background}#{IO.ANSI.black}"
   def hole(_),         do: "#{IO.ANSI.black_background}#{IO.ANSI.white}"
@@ -10,7 +18,7 @@ defmodule Landscape.Display do
   def water(_),        do: "#{IO.ANSI.blue_background}#{IO.ANSI.black}"
 
   def dirt(:winter),   do: "#{IO.ANSI.white_background}#{IO.ANSI.black}"
-  def dirt(_),         do: "#{IO.ANSI.magenta_background}#{IO.ANSI.black}"
+  def dirt(_),         do: "\e[48;5;#{rgb216(1, 1, 0)}m"
 
   def grass(:fall),    do: "#{IO.ANSI.yellow_background}#{IO.ANSI.black}"
   def grass(:winter),  do: "#{IO.ANSI.white_background}#{IO.ANSI.black}"
@@ -19,9 +27,13 @@ defmodule Landscape.Display do
   def flower(:winter), do: "#{IO.ANSI.white_background}#{IO.ANSI.black}#{IO.ANSI.faint}"
   def flower(_),       do: "#{IO.ANSI.green_background}#{IO.ANSI.yellow}"
 
+  def topleft(h),   do: "\e[#{h+3}F"
+
   def print_map({{w, h, m}, date}) do
-    map_w_index = Enum.with_index(m)
-    Enum.each(map_w_index, fn({c, i}) -> print_cell(c, date, Landscape.Position.eol(w, h, i)) end)
+    if date != Calendar.first_day, do: IO.puts topleft(h)
+    m
+    |> Enum.with_index
+    |> Enum.each(fn({c, i}) -> print_cell(c, date, Landscape.Position.eol(w, h, i)) end)
     IO.puts Calendar.date_format(date)
     {{w, h, m}, date}
   end
